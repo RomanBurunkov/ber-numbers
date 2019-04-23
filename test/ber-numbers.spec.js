baeaae568505ea2082732f037e12a1b33789dcbf
Roman Burunkov <romon2002@gmail.com>
2019-04-23 13:41:32 +0300

    Fix errors and add soem tests.

--- test/ber-numbers.spec.js
+++ test/ber-numbers.spec.js
@@ -1,29 +1,33 @@
-const ber = require('./../lib/ber-numbers');
+/* eslint-env jest */
 
-describe('Test length function', () => {
-  test('Should return 1 for values less then 128', () => {
-    ['1', '10', '40', '100', '127'].forEach(val => expect(ber.length(val)).toBe(1));
+const ber = require("./../lib/ber-numbers");
+
+describe("Test length function", () => {
+  test("Should return 1 for values less then 128", () => {
+    ["1", "10", "40", "100", "127"].forEach(val =>
+      expect(ber.length(val)).toBe(1)
+    );
   });
 
-  test('Should return 4 for 83XXXXXX values', () => {
-    expect(ber.length('83000555')).toBe(4);
+  test("Should return 4 for 83XXXXXX values", () => {
+    expect(ber.length("83000555")).toBe(4);
   });
-  
-  test('Should return false if nothing was passed', () => {
+
+  test("Should return false if nothing was passed", () => {
     expect(ber.length()).toBe(false);
   });
 
-  test('Should return false if empty string was passed', () => {
-    expect(ber.length('')).toBe(false);
+  test("Should return false if empty string was passed", () => {
+    expect(ber.length("")).toBe(false);
   });
 
-  test('Should return false if not valid value was passed', () => {
-    expect(ber.length('86000555')).toBe(false);
+  test("Should return false if not valid value was passed", () => {
+    expect(ber.length("86000555")).toBe(false);
   });
 });
 
-describe('Test calcLength function', () => {
-  [1, 255, 65535, 100000, 16777216].forEach((val, index) =>{
+describe("Test calcLength function", () => {
+  [1, 255, 65535, 100000, 16777216].forEach((val, index) => {
     test(`Should return ${index + 1} for ${val}`, () => {
       expect(ber.calcLength(val)).toBe(index + 1);
     });
@@ -32,37 +36,41 @@ describe('Test calcLength function', () => {
   test(`Should return false for values greater than 4294967296`, () => {
     expect(ber.calcLength(4294967297)).toBe(false);
   });
-
 });
 
-describe('Test decode function', () => {
+describe("Test decode function", () => {
   [
-    {encoded: '', decoded: false},
-    {encoded: '1F', decoded: 31},
-    {encoded: '820500', decoded: 1280},
-    {encoded: '83000500', decoded: 1280}
-  ].forEach((testValue) => {
+    { encoded: "", decoded: false },
+    { encoded: "1F", decoded: 31 },
+    { encoded: "81FF", decoded: 255 },
+    { encoded: "820500", decoded: 1280 },
+    { encoded: "83000500", decoded: 1280 }
+  ].forEach(testValue => {
     test(`Should return ${testValue.decoded} for ${testValue.encoded}`, () => {
       expect(ber.decode(testValue.encoded)).toBe(testValue.decoded);
     });
   });
-})
+});
 
-describe('Test encode function', () => {
+describe("Test encode function", () => {
   [
-    {encoded: 0, decoded: false, base: undefined},
-    {encoded: 0, decoded: undefined, base: undefined},
-    {encoded: 0, decoded: null, base: undefined},
-    {encoded: 0, decoded: '', base: undefined},
-    {encoded: 0x1F, decoded: 31, base: undefined},
-    {encoded: 0x81FA, decoded: 250, base: undefined},
-    {encoded: 0x820500, decoded: 1280, base: undefined},
-    {encoded: 0x83000500, decoded: 1280, base: 4},
-    {encoded: 0x83010000, decoded: 65536, base: undefined},
-    {encoded: 0x8401000000, decoded: 16777216, base: undefined},
-  ].forEach((testValue) => {
-    test(`Should return ${testValue.encoded} for ${testValue.decoded}, base: ${testValue.base}`, () => {
-      expect(ber.encode(testValue.decoded, testValue.base)).toBe(testValue.encoded);
+    { encoded: 0, decoded: false, base: undefined },
+    { encoded: 0, decoded: undefined, base: undefined },
+    { encoded: 0, decoded: null, base: undefined },
+    { encoded: 0, decoded: "", base: undefined },
+    { encoded: 0x1f, decoded: 31, base: undefined },
+    { encoded: 0x81fa, decoded: 250, base: undefined },
+    { encoded: 0x820500, decoded: 1280, base: undefined },
+    { encoded: 0x83000500, decoded: 1280, base: 4 },
+    { encoded: 0x83010000, decoded: 65536, base: undefined },
+    { encoded: 0x8401000000, decoded: 16777216, base: undefined }
+  ].forEach(testValue => {
+    test(`Should return ${testValue.encoded} for ${testValue.decoded}, base: ${
+      testValue.base
+    }`, () => {
+      expect(ber.encode(testValue.decoded, testValue.base)).toBe(
+        testValue.encoded
+      );
     });
   });
 
@@ -73,4 +81,4 @@ describe('Test encode function', () => {
   test(`Should return false for value more then 4294967295`, () => {
     expect(ber.encode(4294967296)).toBe(false);
   });
-})
+});
